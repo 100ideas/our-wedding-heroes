0f3d5b73bbbc94947dcc2f9f8b1f8c30d12d3967
Mac Cowell <mac@has100ideas.com>
2016-06-14 22:51:57 -0700

    guess openshift env vars

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    url: process.env.URL || 'http://localhost:8080',
-    port: process.env.PORT || 8080,
-    database: process.env.DATABASE_URL || 'mongodb://localhost:27017/our-wedding-heroes',
+    url: process.env.URL || process.env.OPENSHIFT_NODEJS_IP + ':' + process.env.OPENSHIFT_NODEJS_PORT || 'http://localhost:8080',
+    port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    database: process.env.OPENSHIFT_MONGODB_DB_URL + '/' + process.env.OPENSHIFT_APP_NAME || 'mongodb://localhost:27017/our-wedding-heroes',
     secret: process.env.SECRET || 'ilovetheideaofmywedding',
     siteTitle: process.env.SITE_TITLE || 'Our Wedding',
     paypalMeUsername: process.env.PAYPAL_ME_USERNAME,
