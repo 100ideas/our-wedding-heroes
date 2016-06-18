106b5d671ac80504bdef6a90daaa440379e71c5d
Mac Cowell <mac@has100ideas.com>
2016-06-18 00:16:51 -0700

    fix typo in default mongo db

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     url: process.env.URL || 'http://localhost:8080',
     port: process.env.PORT || 8080,
-    database: process.env.MONGO_URL || 'mongodb://localhost:27017/our-wedding-heroes',
+    database: process.env.MONGO_URL || 'mongodb://localhost:27017/macnlisa',
     secret: process.env.SECRET || 'ilovetheideaofmywedding',
     siteTitle: process.env.SITE_TITLE || 'Mac n Lisa Tie the Knot - 23 July 2016',
     paypalMeUsername: process.env.PAYPAL_ME_USERNAME,
